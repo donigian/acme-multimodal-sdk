0ccfe9d7a3a9f6077b1d956f7ce260935af016ac
Armen Donigian <donigian@gmail.com>
2024-07-21 06:43:31 +0000

    Add functionality to plot multiple images in a grid format.

--- multimodal/src/datasets/plot.py
+++ multimodal/src/datasets/plot.py
@@ -1,22 +1,64 @@
-import matplotlib.pyplot as plt
+import pathlib
+import torch
 import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
+from torchvision import utils
+from torchvision.io import read_image
+from torchvision.utils import make_grid
+from torchvision.transforms import functional as F
+from typing import List
+plt.rcParams["savefig.bbox"] = 'tight'
 
-class Plot:
-  def __init__(self):
-    pass
+class ImagePlotter:
+    """
+    A class for plotting images and image grids.
+    """
 
-  def plot(self, image_path, image_title = ""):
-    img = Image.open(image_path)
-    img_array = np.array(img)
+    def __init__(self):
+        pass
 
-    # Create a figure and axis
-    fig, ax = plt.subplots()
+    def plot_image(self, image_path: str, title: str = "") -> None:
+        """
+        Plots a single image from a given path.
 
-    # Display the image
-    ax.imshow(img_array)
+        Args:
+            image_path (str): Path to the image file.
+            title (str, optional): Title for the plot. Defaults to "".
+        """
+        img = Image.open(image_path)
+        img_array = np.array(img)
 
-    ax.set_title(image_title)
-    ax.axis('off')
+        fig, ax = plt.subplots()
+        ax.imshow(img_array)
 
-    plt.show()
+        ax.set_title(title)
+        ax.axis('off')
+
+        plt.show()
+
+    def plot_images(self, images) -> None:
+        """
+        Plots a list of images in a grid format.
+
+        Args:
+            images: List of images (tensors or NumPy arrays).
+        """
+        if not isinstance(images, list):
+            imgs = [images]
+        fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+        for i, img in enumerate(imgs):
+            img = img.detach()
+            img = F.to_pil_image(img)
+            axs[0, i].imshow(np.asarray(img))
+            axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+            
+    def plot_image_paths(self, image_paths: List[str]) -> None:
+        """
+        Plots a list of images from their paths.
+
+        Args:
+            image_paths (List[str]): List of image file paths.
+        """
+        images = [read_image(path) for path in image_paths]
+        self.plot_images(make_grid(images))
