f8de39ff169f9dcccdac54d955d4b80f3fe1b708
Armen Donigian <donigian@gmail.com>
2024-07-28 05:51:31 +0000

    Added support to detect 9 common issues impacting image data (#5).

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # multimodal-sdk
+
+
+### Find common issues with images
+Here is a list of the common image issue types this SDK supports:
+
+| Issue Type | Issue Description 2 | Issue Key |
+|---|---|---|
+| Light | Images that are too bright/washed out in the dataset | light|
+|Dark |Images that are irregularly dark | dark|
+|Odd Aspect Ratio |Images with an unusual aspect ratio (i.e. overly skinny/wide) | odd_aspect_ratio|
+|Exact Duplicates |Images that are exact duplicates of each other | exact_duplicates|
+|Near Duplicates |Images that are almost visually identical to each other (e.g. same image with different filters) | near_duplicates|
+|Blurry |Images that are blurry or out of focus | blurry|
+|Grayscale |Images that are grayscale (lacking color) | grayscale|
+|Low Information |Images that lack much information (e.g. a completely black image with a few white dots) | low_information|
+|Odd Size |Images that are abnormally large or small compared to the rest of the dataset | aodd_size|
+
+Sample Usage...
+```
+img_utils = ImageUtils(image_dir = "./images")
+img_utils.find_images_in_directory()
+img_utils.find_issues_in_images()
+issue_types_found = img_utils.report_image_issue_types_found()
+
+img_utils.get_list_of_images_no_issues()
+```
---
+++ multimodal/src/datasets/image_utils.py
@@ -0,0 +1,90 @@
+from cleanvision import Imagelab
+from typing import List
+import os
+import pandas as pd
+
+class ImageUtils:
+  """
+  A class for finding issues with images.
+  """
+
+  def __init__(self, image_dir : str):
+    self.image_dir = image_dir
+    self.image_examiner = None
+    self.file_to_issue_map = {}
+    self.all_images = []
+    self.images_no_issues = None
+    self.issue_types = {"dark": {}, "blurry": {}, "exact_duplicates": {},
+              "near_duplicates": {"hash_type": "phash"}, "low_information": {}, "light": {},
+              "grayscale": {}, "odd_aspect_ratio": {}}  
+
+  def find_images_in_directory(self):
+    """
+    Finds images in a directory.
+
+    Args:
+      image_dir: The directory containing the images.
+
+    Returns:
+      A list of images.
+    """
+    image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
+    for filename in os.listdir(self.image_dir):
+      if any(filename.lower().endswith(ext) for ext in image_extensions):
+        self.all_images.append(filename)
+    return self.all_images
+
+  def find_issues_in_images(self) -> None:
+    """
+    Finds different types of issues commonly found in images.
+    """
+    self.image_examiner = Imagelab(data_path=self.image_dir)
+    self.image_examiner.find_issues(issue_types=self.issue_types)
+
+  def report_image_issue_types_found(self) -> pd.DataFrame:
+    """
+    Finds different types of issues commonly found in images.
+
+    Returns:
+      A dataframe of issues found in directory of images.
+    """
+    self.issue_types_found = self.image_examiner.issue_summary.query("num_images > 0")["issue_type"].tolist()
+    return self.image_examiner.issue_summary.query("num_images > 0")
+
+  def set_filenames_with_specific_issue(self, image_issue_type: str) -> None:
+    """
+    Finds images which contain specific type of issue.
+
+    Args:
+      image_issue_type: Specific type of image issue.
+
+    Returns:
+      A list of filenames of images which contain offending issue.
+    """
+    list_of_offending_images = self.image_examiner.issues[self.image_examiner.issues[f"is_{image_issue_type}_issue"] == True].index.tolist()
+    self.file_to_issue_map[image_issue_type] = list_of_offending_images
+
+  def get_list_of_images_no_issues(self) -> List[str]:
+    issue_types_found = self.image_examiner.issue_summary.query("num_images > 0")["issue_type"].tolist()
+    print(f" issue_types_found : {issue_types_found}")
+    for issue in issue_types_found:
+      self.set_filenames_with_specific_issue(issue)
+
+    self.images_no_issues = list(set(self.all_images) - set(list(self.file_to_issue_map.values())[0]))
+    return self.images_no_issues
+
+  def walk_direcotry_assign_labels_by_image_paths(self):
+    """
+    Walks through a directory and assigns labels to images based on their paths.
+
+    """
+    pass
+
+  def labels_to_dataset(self):
+    """
+    Converts labels to dataset.
+
+    Returns:
+      A dataset of labels.
+    """
+    pass
