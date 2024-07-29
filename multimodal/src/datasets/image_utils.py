6653d0df9ea89bab1468c45ca522480c6af873a7
Armen Donigian <donigian@gmail.com>
2024-07-29 04:13:39 +0000

    Fixed bug preventing images with issues from being filtered (#5).

--- multimodal/src/datasets/image_utils.py
+++ multimodal/src/datasets/image_utils.py
@@ -1,7 +1,8 @@
-from cleanvision import Imagelab
-from typing import List
 import os
 import pandas as pd
+from typing import List, Set
+
+from cleanvision import Imagelab
 
 class ImageUtils:
   """
@@ -18,15 +19,12 @@ class ImageUtils:
               "near_duplicates": {"hash_type": "phash"}, "low_information": {}, "light": {},
               "grayscale": {}, "odd_aspect_ratio": {}}  
 
-  def find_images_in_directory(self):
+  def find_images_in_directory(self) -> List[str]:
     """
     Finds images in a directory.
 
-    Args:
-      image_dir: The directory containing the images.
-
     Returns:
-      A list of images.
+      A list of image filenames.
     """
     image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
     for filename in os.listdir(self.image_dir):
@@ -41,12 +39,6 @@ class ImageUtils:
     self.image_examiner = Imagelab(data_path=self.image_dir)
     self.image_examiner.find_issues(issue_types=self.issue_types)
 
-  def visualize_images_with_issues():
-    """
-    Produce a report with issues found in images
-    """
-    return self.image_examiner.report(issue_types=self.issue_types)
-
   def report_image_issue_types_found(self) -> pd.DataFrame:
     """
     Finds different types of issues commonly found in images.
@@ -59,24 +51,27 @@ class ImageUtils:
 
   def set_filenames_with_specific_issue(self, image_issue_type: str) -> None:
     """
-    Finds images which contain specific type of issue.
+    Store mapping of image and the type of issues it contains.
 
     Args:
       image_issue_type: Specific type of image issue.
-
-    Returns:
-      A list of filenames of images which contain offending issue.
     """
     list_of_offending_images = self.image_examiner.issues[self.image_examiner.issues[f"is_{image_issue_type}_issue"] == True].index.tolist()
     self.file_to_issue_map[image_issue_type] = list_of_offending_images
 
   def get_list_of_images_no_issues(self) -> List[str]:
+    """
+    Remove images with issues from original set of images.
+
+    Returns:
+      A list of images which don't contain any issues.
+    """
     issue_types_found = self.image_examiner.issue_summary.query("num_images > 0")["issue_type"].tolist()
     print(f" issue_types_found : {issue_types_found}")
     for issue in issue_types_found:
       self.set_filenames_with_specific_issue(issue)
-
-    self.images_no_issues = list(set(self.all_images) - set(list(self.file_to_issue_map.values())[0]))
+    formatted_set = self._remove_prefix_from_set(set(list(self.file_to_issue_map.values())[0]))
+    self.images_no_issues = list( set(self.all_images) - formatted_set )
     return self.images_no_issues
 
   def walk_direcotry_assign_labels_by_image_paths(self):
@@ -94,3 +89,15 @@ class ImageUtils:
       A dataset of labels.
     """
     pass
+
+  def _remove_prefix_from_set(self, original_set, prefix="/content/images/") -> Set:
+    """Removes the given prefix from each string in the set.
+
+    Args:
+      original_set: The set of strings.
+      prefix: The prefix to remove.
+
+    Returns:
+      A new set with the prefix removed from each string.
+    """
+    return {s[len(prefix):] if s.startswith(prefix) else s for s in original_set}
